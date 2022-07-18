8748a3f79aac74fb1c4716375aac6af18a6874a6
Catherine Lee <csl@fb.com>
2022-07-18 16:30:51 -0700

    -e

--- .github/scripts/monitor.py
+++ .github/scripts/monitor.py
@@ -1,10 +1,11 @@
 import psutil
 import time
 
+
 def main():
-    while True:
-        print(psutil.cpu_percent())
-        time.sleep(.5)
+    print(psutil.cpu_percent())
+    time.sleep(.5)
+
 
 if __name__ == "__main__":
     main()
