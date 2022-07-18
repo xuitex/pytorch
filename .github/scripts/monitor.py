1aa03833cfd517a2bf369bc94c6f41d6f9b7b96f
Catherine Lee <csl@fb.com>
2022-07-18 16:58:16 -0700

    pip to log

--- .github/scripts/monitor.py
+++ .github/scripts/monitor.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
 import psutil
 import time
 
 
-def main():
-    print(psutil.cpu_percent())
-    time.sleep(.5)
+def main() -> None:
+    while True:
+        print(psutil.cpu_percent())
+        time.sleep(.5)
 
 
 if __name__ == "__main__":
