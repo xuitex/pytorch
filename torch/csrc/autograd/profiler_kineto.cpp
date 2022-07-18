ca3a391f835d7bb1a79be8992286e781ef9329e7
Taylor Robie <taylorrobie@fb.com>
2022-07-18 15:06:24 -0700

    [Profiler] Clean up visit logic
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/80822
    
    It's rather tedious to constantly have to specify `extra_fields_` in visit; especially since it tends to add a line. The `DEFINE_VISITOR` logic was also getting rather unwieldy and hard to read. Positional arguments in macros are quite bug prone. I think the new way is clearer.
    ghstack-source-id: 161831723
    
    Differential Revision: [D37481560](https://our.internmc.facebook.com/intern/diff/D37481560/)

--- torch/csrc/autograd/profiler_kineto.cpp
+++ torch/csrc/autograd/profiler_kineto.cpp
@@ -77,7 +77,7 @@ struct EventFieldsVisitor {
     });
 
     pushPythonMetadata(result->parent_.lock());
-    c10::visit(*this, result->extra_fields_);
+    result->visit(*this);
     handleStack(result->parent_);
   }
 
@@ -201,7 +201,7 @@ struct EventFieldsVisitor {
     };
 
     while (parent != nullptr) {
-      c10::visit(push, parent->extra_fields_);
+      parent->visit(push);
       parent = parent->parent_.lock();
     }
   }
