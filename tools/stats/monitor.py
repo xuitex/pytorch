7474a02ea2356bdc233b72d9a9d8ebd061a7345e
Catherine Lee <csl@fb.com>
2022-07-18 18:35:31 -0700

    get around assert git dirty

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+import subprocess
+import sys
 import time
 
 import psutil  # type: ignore[import]
 
 
 def main() -> None:
+    if (
+        subprocess.run(
+            [sys.executable, "-m", "psutil", "--help"], capture_output=True
+        ).returncode
+        != 0
+    ):
+        subprocess.run(
+            [sys.executable, "-m", "pip", "install", "psutil"], capture_output=True
+        )
     while True:
         print(psutil.cpu_percent())
         time.sleep(0.5)
