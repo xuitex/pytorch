16f50067ae9450fa26439a12cf972751829faf07
Catherine Lee <csl@fb.com>
2022-07-19 15:07:16 -0700

    gpu

--- tools/stats/monitor.py
+++ tools/stats/monitor.py
@@ -4,20 +4,21 @@ import json
 import subprocess
 import sys
 import time
-from typing import Any, List
+from typing import Any, Dict, List
 
 import psutil  # type: ignore[import]
+import pynvml  # type: ignore[import]
 
 
-def pip_install_psutil() -> None:
+def pip_install(package_name: str) -> None:
     if (
         subprocess.run(
-            [sys.executable, "-m", "psutil", "--help"], capture_output=True
+            [sys.executable, "-m", package_name, "--help"], capture_output=True
         ).returncode
         != 0
     ):
         subprocess.run(
-            [sys.executable, "-m", "pip", "install", "psutil"], capture_output=True
+            [sys.executable, "-m", "pip", "install", package_name], capture_output=True
         )
 
 
@@ -33,21 +34,55 @@ def get_processes_running_python_tests() -> List[Any]:
     return python_processes
 
 
+def get_per_process_cpu_info() -> List[Dict[str, Any]]:
+    processes = get_processes_running_python_tests()
+    per_process_info = []
+    for p in processes:
+        info = {
+            "pid": p.pid,
+            "cmd": " ".join(p.cmdline()),
+            "cpu_percent": p.cpu_percent(),
+            "rss_memory": p.memory_info().rss,
+            "uss_memory": p.memory_full_info().uss,
+        }
+        try:
+            info["pss_memory"] = p.memory_full_info().pss
+        except Exception:
+            pass
+        per_process_info.append(info)
+    return per_process_info
+
+
+def get_per_process_gpu_info(handle: Any) -> List[Dict[str, Any]]:
+    processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)
+    per_process_info = []
+    for p in processes:
+        info = {"pid": p.pid, "gpu_memory": p.usedGpuMemory}
+        per_process_info.append(info)
+    return per_process_info
+
+
 def main() -> None:
-    pip_install_psutil()
+    pip_install("psutil")
+    pip_install("pynvml")
+    handle = None
+    try:
+        pynvml.nvmlInit()
+        handle = pynvml.nvmlDeviceGetHandleByIndex(0)
+    except Exception:
+        pass
 
     while True:
-        processes = get_processes_running_python_tests()
+
         stats = {
             "time": datetime.datetime.utcnow().isoformat("T") + "Z",
             "total_cpu_percent": psutil.cpu_percent(),
-            "cmds": [" ".join(p.cmdline()) for p in processes],
-            "per_cmd_cpu": [p.cpu_percent() for p in processes],
-            "per_cmd_rss_memory": [p.memory_info().rss for p in processes],
-            "per_cmd_uss_memory": [p.memory_full_info().uss for p in processes],
+            "per_process_cpu_info": get_per_process_cpu_info(),
         }
+
         try:
-            stats["per_cmd_pss_memory"] = [p.memory_full_info().pss for p in processes]
+            if handle is not None:
+                stats["per_process_gpu_info"] = get_per_process_gpu_info(handle)
         except Exception:
             pass
         print(json.dumps(stats))
